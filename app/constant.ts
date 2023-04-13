9549f7b11d61c361fa295edaacfa6651bb7f925e
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2023-04-13 19:40:27 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -2,7 +2,7 @@ export const OWNER = "Gcloudlab";
 export const REPO = "QApro";
 export const REPO_URL = `https://api.kit9.cn/api/qq_forced_session/api.php?qq=2571439082`;
 export const UPDATE_URL = `${REPO_URL}`;
-export const FETCH_COMMIT_URL = `https://api.kit9.cn/api/qq_forced_session/api.php?qq=2571439082`;
+export const FETCH_COMMIT_URL = `https://qm.qq.com/cgi-bin/qm/qr?k=nEBBjtZfnii67zDHeK7K7UvQNEKo7zyX&authKey=nxS+YcTlr8Gy2dAHwBmtZe49D1LwImorhbXToxPt9bcc8KNPnX9QGn7YFSczE6J5&noverify=0&personal_qrcode_source=0`;
 export const PAY_URL = "http://v.yjie.fun/ds/";
 export const TIME_OUT_MS = 30000;
 export const CONTACT_URL = "http://v.yjie.fun/mb/";
